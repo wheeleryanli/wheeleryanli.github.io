45b819d0a46e8924a2e7e522f4fc714bf5de357b
Sun Wenbo <wheeler2023@proton.me>
2026-05-02 21:35:59 -0900

    Load physics particle background assets

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,23 @@
 var posts=["2025/03/27/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
+(function loadPhysicsParticleField(){
+  if (window.__physicsParticleAssetsLoaded) return;
+  window.__physicsParticleAssetsLoaded = true;
+
+  var cssHref = "/css/physics-particles.css";
+  if (!document.querySelector('link[href="' + cssHref + '"]')) {
+    var link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.href = cssHref;
+    document.head.appendChild(link);
+  }
+
+  var scriptSrc = "/js/physics-particles.js";
+  if (!document.querySelector('script[src="' + scriptSrc + '"]')) {
+    var script = document.createElement("script");
+    script.src = scriptSrc;
+    script.defer = true;
+    document.head.appendChild(script);
+  }
+})();
